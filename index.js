e18d372ccfb71e3c8a305d3ccd58af0f3ef43ada
faishal-developer <foysal.professional@gmail.com>
2021-11-27 23:40:42 +0600

    first commit

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const { MongoClient } = require( 'mongodb' );
 const ObjectId = require( 'mongodb' ).ObjectId;
 const app = express()
 const port = process.env.PORT || 5000
+const fileUpload = require( 'express-fileupload' )
 
 //middleWares
 app.use( cors() )
 app.use( express.json() )
+app.use( fileUpload() )
 
 console.log( process.env.DB_PASS, process.env.DB_USER );
 const uri = `mongodb+srv://ass12:iwl4SabFlkpnMve0@cluster0.yaqej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -29,7 +31,11 @@ async function run() {
         } )
         app.post( "/bycycles", async ( req, res ) => {
             const product = req.body;
-
+            const image = req.files.image;
+            const imageData = image.data;
+            const encodePic = imageData.toString( 'base64' )
+            const imageBuffer = Buffer.from( encodePic, 'base64' )
+            product.image = imageBuffer;
             const result = await cycles.insertOne( product )
             res.json( result );
         } )
@@ -37,6 +43,12 @@ async function run() {
             const cursor = orders.find( {} );
             const result = await cursor.toArray()
             res.json( result );
+        } )
+        app.delete( "/orders", async ( req, res ) => {
+            let id = req.body.id
+            const query = { _id: ObjectId( id ) };
+            const result = await orders.deleteOne( query );
+            res.json( result )
         } )
         app.post( "/getOrders", async ( req, res ) => {
             const email = req.body.email
@@ -58,7 +70,6 @@ async function run() {
         } )
         app.post( "/reviews", async ( req, res ) => {
             const review = req.body;
-
             const result = await reviews.insertOne( review )
             res.json( result );
         } )
@@ -71,6 +82,7 @@ async function run() {
         } )
         app.put( "/users", async ( req, res ) => {
             const email = req.body.email;
+            console.log( email, 'working' );
             const filter = { email };
             const options = { upsert: false };
             const updateDoc = {
@@ -78,7 +90,20 @@ async function run() {
                     role: 'admin'
                 },
             };
+
             const result = await users.updateOne( filter, updateDoc, options );
+            res.json( result );
+        } )
+        app.put( "/orders", async ( req, res ) => {
+            const id = req.body.id
+            const filter = { _id: ObjectId( id ) };
+            const options = { upsert: false };
+            const updateDoc = {
+                $set: {
+                    status: 'shipted'
+                },
+            };
+            const result = await orders.updateOne( filter, updateDoc, options );
             res.json( result );
         } )
         app.get( "/users/:email", async ( req, res ) => {
