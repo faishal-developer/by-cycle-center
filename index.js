9d151e096414c064209ba1e9bd31dd37bec7c832
faishal-developer <foysal.professional@gmail.com>
2023-11-22 11:24:20 +0600

    kdkfj

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express')
 const cors = require('cors')
+const env=require('dotenv');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const app = express()
+// require('dotenv').config()
+env.config()
 const port = process.env.PORT || 5000
 const fileUpload = require('express-fileupload')
-
+// w7D0qgw4z0XvtcO1
 //middleWares
+// mdfaishal329
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload())
 
 console.log(process.env.DB_PASS, process.env.DB_USER);
-const uri = `mongodb+srv://ass12:iwl4SabFlkpnMve0@cluster0.yaqej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7rmmpa.mongodb.net/?retryWrites=true&w=majority`; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
         const database = client.db("by_cycle_center");
+        console.log("database connected");
         const cycles = database.collection("by_cycles");
         const reviews = database.collection("reviews");
         const users = database.collection("users");
         const orders = database.collection("orders");
 
         app.get("/bycycles", async (req, res) => {
+            console.log("dkdifjdffkj");
             const cursor = cycles.find({});
             const result = await cursor.toArray()
             res.json(result);
